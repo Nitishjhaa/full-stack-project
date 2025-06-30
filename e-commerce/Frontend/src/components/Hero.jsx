2b567c5a49ba2eb4c73bbc09d60e268fa1af1f7e
Nitishjhaa <nj9876543210p@gmail.com>
2025-06-30 22:27:47 +0530

    made some more progress in 30/06

--- e-commerce/Frontend/src/components/Hero.jsx
+++ e-commerce/Frontend/src/components/Hero.jsx
@@ -4,12 +4,12 @@ import { assets } from '../assets/frontend_assets/assets'
 const Hero = () => {
   return (
     <div className='flex flex-col md:flex-row border border-gray-400 h-130'>
-      <div className='w-1/2 flex h-full justify-center items-center flex-col max-md:w-full'>
+      <div className='w-1/2 flex h-full justify-center items-center flex-col max-md:w-full max-md:mb-10 max-md:mt-10'>
         <div className='flex gap-2 mr-37 mb-4'>
           <p className='w-8 md:w-11 h-[1px] bg-[#414141] mt-3 '></p>
           <p className='font-medium text-sm md:text-base uppercase'>Our bestseller</p>
         </div>
-        <div className='text-5xl prata-regular text-[#414141]'>
+        <div className='text-5xl prata-regular text-[#414141] whitespace-nowrap max-md:text-4xl max-md:mr-14'>
           Latest Arrivals
         </div>
         <div className='flex gap-2 mr-48 text-sherif mt-4'>
