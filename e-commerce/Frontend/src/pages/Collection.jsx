6f50b920d85f45d550ff67f8f69e438bab3a3c3b
Nitishjhaa <nj9876543210p@gmail.com>
2025-08-03 22:24:52 +0530

    startted this project again

--- e-commerce/Frontend/src/pages/Collection.jsx
+++ e-commerce/Frontend/src/pages/Collection.jsx
@@ -1,19 +1,25 @@
 import React, { useState, useContext, useEffect } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import { assets } from '../assets/frontend_assets/assets'
+import ProductItems from '../components/ProductItems'
+import Title from '../components/Title'
 
 const Collection = () => {
 
   const { products } = useContext(ShopContext)
   const [showFilter, setShowFilter] = useState(false)
 
+  const [allProducts, setAllProducts] = useState([])
 
+  useEffect(() => {
+    setAllProducts(products)
+  }, [])
 
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 border-t' >
 
       {/* filter tag */}
-      <div className='min-w-60'>
+      <div className='min-w-60 mt-5'>
         <p className='uppercase my-2 text-xl flex items-center cursor-pointer gap-2' onClick={() => setShowFilter(!showFilter)}>Filter
           <img className={`h-3 sm:hidden ${showFilter ? "rotate-90 duration-300 " : "duration300"}`} src={assets.dropdown_icon} alt="" />
         </p>
@@ -51,7 +57,24 @@ const Collection = () => {
 
       {/* right side */}
       <div className='flex-1 '>
-        
+        <div className='flex justify-between text-base sm:text-2xl my-5'>
+          <Title text1={"All"} text2={"Collection"} />
+          {/* product sort */}
+          <div>
+            <select className='border-2 border-gray-300 text-sm px-2 py-3'>
+              <option value="relevent">Sort by : Relevent</option>
+              <option value="low-high">Sort by : Low-high</option>
+              <option value="high-low">Sort by : High-low</option>
+            </select>
+          </div>
+
+        </div>
+        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 gap-y-6'>
+          {allProducts.map((item, index) => (
+            <ProductItems key={index} id={item.id} image={item.image} name={item.name} price={item.price} />
+          ))
+          }
+        </div>
       </div>
 
     </div >
