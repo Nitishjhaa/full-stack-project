19c1448a015e21b05b767ea5fc5d0750b1cf8c57
Nitishjhaa <nj9876543210p@gmail.com>
2025-08-12 22:40:25 +0530

    made filter and sorting working

--- e-commerce/Frontend/src/pages/Collection.jsx
+++ e-commerce/Frontend/src/pages/Collection.jsx
@@ -9,47 +9,74 @@ const Collection = () => {
   const { products } = useContext(ShopContext)
   const [showFilter, setShowFilter] = useState(false);
   const [allProducts, setAllProducts] = useState([]);
-  const [catogery, setCatogery] = useState([]);
-  const [subCatogery, setSubCatogery] = useState([])
+  const [category, setcategory] = useState([]);
+  const [subcategory, setSubcategory] = useState([]);
+  const [sortType, setSortType] = useState('relevent')
 
-  const toggleCatogery = (e) => {
-    if (catogery.includes(e.target.value)) {
-      setCatogery(prev => prev.filter(item => item !== e.target.value))
+  const togglecategory = (e) => {
+    if (category.includes(e.target.value)) {
+      setcategory(prev => prev.filter(item => item !== e.target.value))
     }
     else {
-      setCatogery(prev => [...prev, e.target.value])
+      setcategory(prev => [...prev, e.target.value])
     }
   }
 
-  useEffect(() => {
-    setAllProducts(products)
-  }, [])
-
-  const toggleSubCatogery = (e) => {
-    if (subCatogery.includes(e.target.value)) {
-      setSubCatogery(prev => prev.filter(item => item !== e.target.value))
+  const toggleSubcategory = (e) => {
+    if (subcategory.includes(e.target.value)) {
+      setSubcategory(prev => prev.filter(item => item !== e.target.value))
     }
     else {
-      setSubCatogery(prev => [...prev, e.target.value])
+      setSubcategory(prev => [...prev, e.target.value])
     }
   }
 
   const applyFilter = () => {
     let productsCopy = products.slice();
 
-    if(catogery.length > 0) {
-      productsCopy = productsCopy.filter(item => catogery.includes(item.catogery) )
+    if (category.length > 0) {
+      productsCopy = productsCopy.filter(item => category.includes(item.category));
     }
-    
+
+    if (subcategory.length > 0) {
+      productsCopy = productsCopy.filter(item => subcategory.includes(item.subCategory));
+    }
+
+    setAllProducts(productsCopy);
   }
 
   useEffect(() => {
-    setAllProducts(products)
-  })
+    setAllProducts(products);
+  }, [products]);
 
   useEffect(() => {
     applyFilter();
-  },[catogery, subCatogery])
+  }, [category, subcategory])
+
+  useEffect(() => {
+    let productsCopy = products.slice();
+
+    if (category.length > 0) {
+      productsCopy = productsCopy.filter(item => category.includes(item.category));
+    }
+
+    if (subcategory.length > 0) {
+      productsCopy = productsCopy.filter(item => subcategory.includes(item.subCategory));
+    }
+
+    switch (sortType) {
+      case 'low-high':
+        productsCopy = productsCopy.sort((a, b) => a.price - b.price);
+        break;
+      case 'high-low':
+        productsCopy = productsCopy.sort((a, b) => b.price - a.price);
+        break;
+      default:
+        break;
+    }
+
+    setAllProducts(productsCopy);
+  }, [category, subcategory, sortType, products]);
 
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 border-t' >
@@ -60,18 +87,18 @@ const Collection = () => {
           <img className={`h-3 sm:hidden ${showFilter ? "rotate-90 duration-300 " : "duration300"}`} src={assets.dropdown_icon} alt="" />
         </p>
 
-        {/* catogery filter */}
+        {/* category filter */}
         <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? "" : "hidden"} sm:block `}>
-          <p className='uppercase font-semibold text-sm mb-3'>Catogery</p>
+          <p className='uppercase font-semibold text-sm mb-3'>category</p>
           <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
             <p className='flex gap-2 font-semibold'>
-              <input type="checkbox" name="" id="" className='w-3' value={'Men'} onChange={toggleCatogery} />Men
+              <input type="checkbox" name="" id="" className='w-3' value={'Men'} onChange={togglecategory} />Men
             </p>
             <p className='flex gap-2 font-semibold'>
-              <input type="checkbox" name="" id="" className='w-3' value={'Women'} onChange={toggleCatogery} />Women
+              <input type="checkbox" name="" id="" className='w-3' value={'Women'} onChange={togglecategory} />Women
             </p>
             <p className='flex gap-2 font-semibold'>
-              <input type="checkbox" name="" id="" className='w-3' value={'Kids'} onChange={toggleCatogery} />Kids
+              <input type="checkbox" name="" id="" className='w-3' value={'Kids'} onChange={togglecategory} />Kids
             </p>
           </div>
         </div>
@@ -79,13 +106,13 @@ const Collection = () => {
           <p className='uppercase font-semibold text-sm mb-3'>Type</p>
           <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
             <p className='flex gap-2 font-semibold'>
-              <input type="checkbox" name="" id="" className='w-3' value={'Topwear'} onChange={toggleSubCatogery} />Topwear
+              <input type="checkbox" name="" id="" className='w-3' value={'Topwear'} onChange={toggleSubcategory} />Topwear
             </p>
             <p className='flex gap-2 font-semibold'>
-              <input type="checkbox" name="" id="" className='w-3' value={'Bottomwear'} onChange={toggleSubCatogery} />Bottomwear
+              <input type="checkbox" name="" id="" className='w-3' value={'Bottomwear'} onChange={toggleSubcategory} />Bottomwear
             </p>
             <p className='flex gap-2 font-semibold'>
-              <input type="checkbox" name="" id="" className='w-3' value={'Winterwear'} onChange={toggleSubCatogery} />Winterwear
+              <input type="checkbox" name="" id="" className='w-3' value={'Winterwear'} onChange={toggleSubcategory} />Winterwear
             </p>
           </div>
         </div>
@@ -97,7 +124,7 @@ const Collection = () => {
           <Title text1={"All"} text2={"Collection"} />
           {/* product sort */}
           <div>
-            <select className='border-2 border-gray-300 text-sm px-2 py-3'>
+            <select onChange={(e) => setSortType(e.target.value)} className='border-2 border-gray-300 text-sm px-2 py-3'>
               <option value="relevent">Sort by : Relevent</option>
               <option value="low-high">Sort by : Low-high</option>
               <option value="high-low">Sort by : High-low</option>
