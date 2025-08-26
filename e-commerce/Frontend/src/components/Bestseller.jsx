f4e0e46cbb766b9fec9324c5027613897592e7dd
Nitishjhaa <nj9876543210p@gmail.com>
2025-08-26 23:22:10 +0530

    made more progress

--- e-commerce/Frontend/src/components/Bestseller.jsx
+++ e-commerce/Frontend/src/components/Bestseller.jsx
@@ -26,7 +26,7 @@ const Bestseller = () => {
       <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
         {
           bestSeller.map((item, index) => (
-            <ProductItems key={index} id = {item.id} image = {item.image} name= {item.name} price={item.price} />
+            <ProductItems key={index} id = {item._id} image = {item.image} name= {item.name} price={item.price} />
           ))
         }
       </div>
--- e-commerce/Frontend/src/pages/Product.jsx
+++ e-commerce/Frontend/src/pages/Product.jsx
@@ -1,15 +1,53 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { ShopContext } from '../context/ShopContext';
+
 
 const Product = () => {
 
   const { productId } = useParams();
+  const { products } = useContext(ShopContext);
+  const [productData, setProductData] = useState(false);
+  const [image, setImage] = useState('');
 
-  return (
-    <div>
+  const fetchProductData = async () => {
+    products.map((i) => {
+      if (i._id === productId) {
+        setProductData(i);
+        console.log(i)
+        setImage(i.image[0])
+        return null;
+      }
+      else {
 
+      }
+    })
+  }
+
+  useEffect(() => { fetchProductData() }, [productId, products])
+
+  return productData ? (
+    <div className='border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100'>
+      {/* product data */}
+      
+      <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>
+
+        {/* product images */}
+        <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
+          <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-hidden justify-between sm:justify-normal sm:w-[18.7%] w-full'>
+            {
+              productData.image.map((item, index) => (
+                <img src={item} alt={item} key={index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' />
+              ))
+            }
+          </div>
+          <div className='w-full sm:w-[100%]'>
+            <img src={image} alt="" />
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  ) : <div className='opacity-0'></div>
 }
 
 export default Product
